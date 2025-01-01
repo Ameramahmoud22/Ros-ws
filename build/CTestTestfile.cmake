759303c58afd328b10633ff60609689dcab3ad96
Ameramahmoud22 <ameraelbassal552005@gmail.com>
2025-01-01 15:51:57 +0200

    upload src folder

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mmabas77/Ros-ws/src
+# Build directory: /home/mmabas77/Ros-ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mmabas77/Ros-ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/mmabas77/Ros-ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/mmabas77/Ros-ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/mmabas77/Ros-ws/src:$ROS_PACKAGE_PATH"
